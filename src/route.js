b06de0aaa8ea3c391480444bc9da02a36fe9b10c
Igor Ferreira <igor.magno@protonmail.com>
2021-06-24 11:20:28 -0300

    implementação de rota para criar sala e edição dos  caminhos para img, scripts e styles

--- src/route.js
+++ src/route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 
 
@@ -8,10 +9,10 @@ const route = express.Router()
 route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
 route.get('/create-pass', (req, res) => res.render('index', { page: 'create-pass' }))
 
-route.get('/room', (req, res) => res.render('room'))
+route.get('/room/:room', (req, res) => res.render('room'))
 
 route.post('/room/:room/:question/:action', QuestionController.index)
-
+route.post('/room/create-room', RoomController.create)
 
 
 module.exports = route
