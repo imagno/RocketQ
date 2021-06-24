ecb85b55dd7ea84faed75b0cc015e4c4e836d9bb
Igor Ferreira <igor.magno@protonmail.com>
2021-06-23 21:50:57 -0300

    excluí a pasta htmls e criei as rotas room e create-pass

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const route = require('./route')
 const server = express()
 
 server.set('view engine', 'ejs')
+
+server.use(express.static('public'))
+
 server.set('views', path.join(__dirname, 'views'))
 
 server.use(route)
